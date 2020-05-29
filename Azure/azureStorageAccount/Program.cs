e1d3f1fca85d0ecf051a6bd970b25f7823ce1e7c
Michael Levan <mlevan1992@gmail.com>
2020-05-29 14:52:32 -0400

    List out storage accounts in Azure

---
+++ Azure/azureStorageAccount/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Fluent;
+using System;
+
+namespace azureStorageAccount
+{
+    public class Program
+    {
+        static void Main() 
+        {
+            var ClientId = "";
+            var ClientSecret = "";
+            var AzureTenantId = "";
+            var subscriptionid = "";
+
+            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(ClientId, ClientSecret, AzureTenantId, AzureEnvironment.AzureGlobalCloud);
+
+            var azure = Microsoft.Azure.Management.Fluent.Azure.Configure().Authenticate(credentials).WithSubscription(subscriptionid);
+
+            foreach (Microsoft.Azure.Management.Storage.Fluent.IStorageAccount i in azure.StorageAccounts.List()) {
+                Console.WriteLine(i.Name);
+            };
+
+        }
+    }
+}
