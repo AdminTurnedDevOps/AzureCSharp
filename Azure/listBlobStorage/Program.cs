d0ae6baadea6bb60eb489967eea5653cf6d3f229
Michael Levan <mlevan1992@outlook.com>
2020-06-02 15:40:46 -0400

    List blobs from a container

---
+++ Azure/listBlobStorage/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+
+namespace listBlobStorage
+{
+    class Program
+    {
+        string storageConnectionString;
+        string containerName;
+
+        static void Main(string[] args)
+        {
+            listBlobStorage();
+        }
+
+        static void listBlobStorage()
+        {
+
+            Console.WriteLine("Enter the storage account connection string");
+            string storageConnectionString = Console.ReadLine();
+
+            var storageConnect = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient blob = storageConnect.CreateCloudBlobClient();
+            
+
+            Console.WriteLine("Enter container name");
+            string containerName = Console.ReadLine();
+            var containerRef = blob.GetContainerReference(containerName);
+            Console.WriteLine(containerRef.ListBlobs());
+
+            foreach (CloudBlockBlob container in containerRef.ListBlobs())
+            {
+                Console.WriteLine(container.Name);
+            }
+
+
+        }
+    }
+}
